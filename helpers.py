3f4dc9fc493915ac774d6cee57ad1ac50063c96b
Dan Shiebler <dshiebler@abnormalsecurity.com>
2022-04-30 14:42:23 -0400

    clean up

--- helpers.py
+++ helpers.py
@@ -1,189 +1,124 @@
+from collections import namedtuple
+
 import numpy as np
 from numpy.polynomial import Polynomial
 import sympy
 from functools import reduce
-from scipy.spatial.distance import cosine
-from tqdm import tqdm
-from collections import namedtuple
-
-NumericType = namedtuple("NumericType", "int rational")
 
 
+Results = namedtuple("Results", "best_vals best_objective_value last_vals last_objective_value")
 
-######################################## Integer Helpers ########################################
+
 def size_one_steps(num_dict, **kwargs):
     """
-    Given an integer-valued vector, return the integer-valued vector that has a maximum dimension magnitude of
-        max_value and points in as similar a direction as possible
+    Given an integer-valued vector, return the integer-valued vector that has a maximum dimension absolute value of
+        1 and points in as similar a direction as possible
     """
     return {key: int(np.sign(num)) for key, num in num_dict.items()}
 
 
-
-def reduce_nums(num_dict, **kwargs):
-    try:
-        """
-        Given an integer-valued vector, return the smallest integer-valued vector that points in the same direction
-        """
-        div = None
-        while div is None or div >= 2:
-            div = np.gcd.reduce(np.array([int(k) for k in num_dict.values()], dtype=np.int32))
-            assert div != 0
-            num_dict = {key: int(num / div) for key, num in num_dict.items()}
-    except TypeError as e:
-        print(num_dict)
-        raise e
-    except OverflowError:
-        return num_dict
-    return num_dict
-
-
-def reduce_nums_approx(num_dict, max_value=10, **kwargs):
+def random_sos_polynomial(
+    max_vars,
+    max_terms,
+    max_symbols_per_term,
+    max_coeff,
+    max_offset
+):
     """
-    Given an integer-valued vector, return the integer-valued vector that has a maximum dimension magnitude of
-        max_value and points in as similar a direction as possible
+    Generate a random sympy polynomial that can be expressed as a sum of squares
+    Args:
+        max_vars: The maximum number of unique variables in the polynomial
+        max_terms: The maximum number of terms in the polynomial
+        max_symbols_per_term: The maximum number of variables in each term
+        max_coeff: The maximum coefficient on any term
+        max_offset: The maximum value of the offset (constant) term
+        max_num_vars: 
+    Returns:
+        A polynomial
     """
-    div = max(num_dict.values()) / max_value
-    if div == 0:
-        return num_dict
-    return {key: int(num / div) for key, num in num_dict.items()}
-
-
-def reduce_nums_log(num_dict, max_value=100, **kwargs):
-    """
-    Given an integer-valued vector, return the integer-valued vector that has a maximum dimension magnitude of
-        max_value and points in as similar a direction as possible
-    """
-    return {key: np.sign(num)*int(np.log(np.abs(1 + float(num)))) for key, num in num_dict.items()}
-
-
-
-def line_search_reduce_nums_approx(num_dict, poly, gen_vals):
-    """
-    Use a line search to find optimal learning rate
-        num_dict: Map from variable to gradient value
-        poly: sympy polynomial to minimize
-        gen_vals: Map from variable to current value
-    """
-    best_objective_fn, best_mv = np.inf, None
-    for mv in [1, 5, 10, 100, 1000]:
-        reduced_num_dict = reduce_nums_approx(num_dict=num_dict, max_value=mv) 
-        objective_fn = poly.eval(
-            {var: gen_vals[var] - reduced_num_dict[var] for var, val in gen_vals.items()})
-        if objective_fn <= best_objective_fn:
-            best_objective_fn = objective_fn
-            best_mv = mv
-    return reduce_nums_approx(num_dict=num_dict, max_value=best_mv) 
-
-
-def line_search_learning_rate(num_dict, poly, gen_vals):
-    """
-    Use a line search to find optimal learning rate
-        num_dict: Map from variable to gradient value
-        poly: sympy polynomial to minimize
-        gen_vals: Map from variable to current value
-    """
-    best_objective_fn, best_lr = np.inf, None
-    for lr in [sympy.Rational(10), sympy.Rational(1), sympy.Rational(0.1), sympy.Rational(0.01), sympy.Rational(0.001)]:
-        objective_fn = poly.eval({var: gen_vals[var] - lr*num_dict[var] for var, val in gen_vals.items()})
-        if objective_fn <= best_objective_fn:
-            best_objective_fn = objective_fn
-            best_lr = lr
-    return {k: best_lr * v for k, v in num_dict.items()}
-
-
-
-
-######################################## Random helpers ########################################
-
-def random_sos_polynomial(max_vars, max_terms, numeric_type, max_symbols_per_term, max_coeff, max_offset):
-    """
-    Generate a random sympy polynomial
-    """
-    num_vars = np.random.randint(1, 10)
-    num_terms = np.random.randint(1, 10)
+    num_vars = np.random.randint(1, max_vars)
+    num_terms = np.random.randint(1, max_terms)
     symbols = [sympy.symbols('x_{}'.format(i)) for i in range(num_vars)]
     poly = 0
     for term in range(num_terms):
-        included_symbols = np.random.choice(symbols, size=1 + int(np.random.random()*max_symbols_per_term), replace=True)
+        included_symbols = np.random.choice(symbols, size=1 + int(np.random.random() * max_symbols_per_term), replace=True)
         variable_term = reduce(lambda a,b: a * b, included_symbols)
 
         # Generate a coefficient
-        if numeric_type == NumericType.int:
-            coeff = int(np.random.random()*max_coeff) + 1
-        elif numeric_type == NumericType.rational:
-            coeff = sympy.Rational(int(np.random.random()*max_coeff) + 1, int(np.random.random()*max_coeff) + 1)
-        else:
-            raise ValueError(numeric_type)
-                
-        # Generate an offset term
-        offset = int(np.random.random()*2*max_offset - max_offset)
-        
+        coeff = int(np.random.random()*max_coeff) + 1
+
+        # Generate an offset term between -max_offset, max_offset
+        offset = np.random.randint(-max_offset, max_offset)
+
         # We square all of the terms since a sum-of-square polynomial is positive in Rn 
         #    https://en.wikipedia.org/wiki/Positive_polynomial#Examples
         poly += (coeff * variable_term + offset)**2
     return poly.as_poly(), poly.as_poly().gens
 
 
-def sample_gen_vals(poly, scale, numeric_type):
+def sample_gen_vals(poly, max_scale=5):
     """
-    Given a sympy polynomial, generate random variable values
+    Given a sympy polynomial, choose scale uniformly from [1, max_scale], then generate a random value for each variable
+        by generating an exponent in (-scale, scale) and selecting an integer in (-2^scale, 2^scale)
     """
+    scale = np.random.randint(low=1, high=max_scale)
     sampled_nums = np.random.randint(
         low=0, high=scale, size=np.array([len(poly.gens)]))
-    raw_nums = [(-1)**(np.random.randint(low=1,high=3)) * (2**s) for s in sampled_nums]
-    if numeric_type == NumericType.int:
-        nums = raw_nums
-    elif numeric_type == NumericType.rational:
-        denominators = np.random.randint(
-            low=1, high=scale, size=np.array([len(poly.gens)]))
-        nums = [sympy.Rational(n, d) for n, d in zip(raw_nums, denominators)]
-    else:
-        raise ValueError(numeric_type)
+    nums = [np.random.choice([-1, 1]) * (2**s) for s in sampled_nums]
     return {g: v for g, v in zip(poly.gens, nums)}
 
 
-
-
-def random_search(poly, numeric_type, steps=1000, scale=10):
+def random_search(poly, steps):
     """
-    Uniformly generate exponents in (-scale, scale), then generates
-        random numbers from (-2^scale, 2^scale) and test them in the polynomial
+    Use sample_gen_vals to randomly generate values and test them in the polynomial
+    Args:
+        poly: The sympy polynomial to optimize
+        steps: Steps to take
+    Returns:
+        optimization results
     """
     best_vals, best_objective_value = None, np.inf
     for i in range(steps):
-        gen_vals = sample_gen_vals(poly=poly, scale=scale, numeric_type=numeric_type)
+        gen_vals = sample_gen_vals(poly=poly)
         objective_value = poly.eval(gen_vals)
         
         # Update if this loss is better than what we have seen before
         if objective_value < best_objective_value:
             best_vals, best_objective_value = gen_vals, objective_value
-    return best_vals, best_objective_value
+    return Results(
+        best_vals=best_vals,
+        best_objective_value=best_objective_value,
+        last_vals=gen_vals,
+        last_objective_value=objective_value)
 
 
-
-
-######################################## Gradient Descent ########################################
-
-def gradient_descent(poly, start, steps, reduce_fn=None, min_value=-1e10, verbosity=None):
+def gradient_descent(
+        poly,
+        steps,
+        reduce_fn=size_one_steps,
+        min_value=-1e50,
+        max_value=1e10,
+        verbosity=None):
     """
     Run gradient descent on a sympy polynomial
-        poly: sympy polynomial
-        start: starting point for gradient descent
-        steps: steps to take
-        reduce_fn: the function to modify the gradient (e.g. multiply by learning rate)
+    Args
+        poly: The sympy polynomial to optimize
+        steps: Steps to take
+        reduce_fn: The function to modify the gradient
+        min_value: The point to stop the optimization because we have converged
+        max_value: The point to stop the optimization because we have diverged
+        verbosity: If not None, the number of iterations at which to log the objective
     Returns:
-        gen_vals: The map from variable to value 
-        best_objective_value
-        final_objective_value: The final objective value
+        Optimization results
     """
+    start = sample_gen_vals(poly)
+
     assert list(start.keys()) == list(poly.gens)
 
     gen_vals = {k: v for k, v in start.items()}
-    best_objective_value = np.inf
+    best_vals, best_objective_value = gen_vals, np.inf
     for i in range(steps):
         objective_value = poly.eval(gen_vals)
-        best_objective_value = min(best_objective_value, objective_value)
 
         if verbosity is not None and i % (steps / verbosity) == 0:
             print(float(objective_value))
@@ -195,39 +130,17 @@ def gradient_descent(poly, start, steps, reduce_fn=None, min_value=-1e10, verbos
         reduced_grad = reduce_fn(num_dict=raw_grad, poly=poly, gen_vals=gen_vals) if reduce_fn is not None else raw_grad
     
         gen_vals = {var: gen_vals[var] - reduced_grad[var] for var, val in gen_vals.items()}
-    return gen_vals, best_objective_value, objective_value
-
-
-def gd_with_restart(poly, restarts, steps, numeric_type, scale, reduce_fn=None, min_value=-1e10, verbosity=None):
-    """
-    Run gradient descent with random restarts on a sympy polynomial
-        poly: sympy polynomial
-        restarts: number of random restarts
-        steps: total steps to take across all restarts
-        numeric_type
-        scale: search scale for random number generation
-        reduce_fn: the function to modify the gradient (e.g. multiply by learning rate)
-    Returns:
-        gen_vals: The map from variable to value 
-        best_objective_value
-    """
-    best_vals, best_objective_value = None, np.inf
-
-    for i in range(restarts):
-        start = sample_gen_vals(poly, scale, numeric_type)
-        gen_vals, objective_value, _ = gradient_descent(
-            poly=poly,
-            start=start,
-            steps=steps,
-            min_value=min_value,
-            verbosity=verbosity,
-            reduce_fn=reduce_fn)
-
-    # Update if this loss is better than what we have seen before
-    if objective_value < best_objective_value:
-        best_vals, best_objective_value = gen_vals, objective_value
-    return best_vals, best_objective_value
+        # Update if this loss is better than what we have seen before
+        if objective_value < best_objective_value:
+            best_vals, best_objective_value = gen_vals, objective_value
         
+    return Results(
+        best_vals=best_vals,
+        best_objective_value=best_objective_value,
+        last_vals=gen_vals,
+        last_objective_value=objective_value)
+
+
 
   
     
--- polynomial_helpers.py
+++ polynomial_helpers.py
@@ -1,81 +1,131 @@
+from collections import namedtuple
+from functools import reduce
+
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy.polynomial import Polynomial
 import sympy
-from functools import reduce
 from scipy.spatial.distance import cosine
 from tqdm import tqdm
-import helpers
 from sympy.solvers.solveset import linsolve
 from sympy.printing.latex import LatexPrinter, print_latex
 
 
-def get_dynamical_system_equation(t, a_dict, dP):
+DynamicalSystemResult = namedtuple("DynamicalSystemResult", "t_values, s_values, coefficient_dict")
+
+
+def get_dynamical_system_result(x, polynomial_to_optimize, max_t):
+    """
+    Args:
+        x: sympy variable
+        polynomial_to_optimize: A sympy polynomial constructed on x
+        max_t: The maximum time step
+    Return:
+        A DynamicalSystemResult describing the trajectory of the optimization process for optimizing
+            polynomial_to_optimize
+    """
+    t_values = range(1, max_t + 1)
+    
+    # Get the equations to solve
+    equations, coefficient_dict = get_equations_from_polynomial(
+        x=x,
+        polynomial_to_optimize=polynomial_to_optimize,
+        max_t=max_t)
+    
+    # Solve the equations
+    solution_dict = get_solution_dict(equations=equations,  coefficient_dict=coefficient_dict)
+    
+    # Extract the system trajectory polynomial from the solution
+    coefficient_dict = {i: solution_dict[coefficient_dict[i]] for i in range(len(solution_dict))}
+    
+    # Evaluate the polynomial at all points
+    s_values = [evaluate_s(t_value=t_value, coefficient_dict=coefficient_dict) for t_value in t_values]
+    return DynamicalSystemResult(t_values=t_values, s_values=s_values, coefficient_dict=coefficient_dict)
+
+
+def get_equations_from_polynomial(x, polynomial_to_optimize, max_t):
+    """
+    Given a polynomial to optimize, compute the set of equations that the solution trajectory would satisfy
+    Args:
+        x: sympy variable
+        polynomial_to_optimize: A sympy polynomial constructed on x
+        max_t: The maximum time step
+    Return:
+        A tuple of (equations to solve, dictionary mapping indices to sympy variables in equation)
+    """
+    P = lambda x: polynomial_to_optimize.as_poly().eval(x)
+    dP = lambda x: polynomial_to_optimize.diff().as_poly().eval(x)    
+
+    # The degree of the polynomial s
+    num_symbols = 2*max_t + 1
+
+    # The coefficients of the polynomial s
+    coefficient_dict = {i: sympy.symbols("a{}".format(i)) for i in range(num_symbols)}
+
+    # The equations to require that the derivative of s is dl/dx
+    dldx_dsdt_equations = [get_dldx_dsdt_equation(t=i, coefficient_dict=coefficient_dict, dP=dP) for i in range(1, max_t + 1)]
+
+    # The equations to require that s passes through gradient descent
+    dynamical_system_equations = [
+        get_dynamical_system_equation(t=i, coefficient_dict=coefficient_dict, dP=dP) for i in range(1, max_t + 1)]
+    return dldx_dsdt_equations + dynamical_system_equations, coefficient_dict
+
+
+def get_dynamical_system_equation(t, coefficient_dict, dP):
     """
     Returns the s(t+1) = s(t) + dl/dx(s(t)) equation that we implicitly set equal to 0. This is the equation that enforces that the integer polynomial s passes through the trajectory of integer gradient descent
         t: integer
-        a_dict: dictionary {index: sympy variable}
+        coefficient_dict: dictionary {index: sympy variable}
         dP: linear function
     
     This returns the following expression, which we interpret as being set equal to 0
         a0(1 - 1 - dP(1)) + a1((t+1) - t - dP(t)) + a2((t+1)^2 - t^2 - dP(t^2)) + ... + an((t+1)^n - t^n - dP(t^n))
     """
     out = 0
-    for i in range(len(a_dict)):
-        out += a_dict[i]*(((t + 1)**i) - (t**i) + dP(t**i))
+    for i in range(len(coefficient_dict)):
+        out += coefficient_dict[i]*(((t + 1)**i) - (t**i) + dP(t**i))
     return out
 
 
-def get_dldx_dsdt_equation(t, a_dict, dP):
+def get_dldx_dsdt_equation(t, coefficient_dict, dP):
     """
     Returns the dl/dx(s(t)) = ds/dx(t) equation that we implicitly set equal to 0. This is the equation that enforces that at t the derivative of the integer polynomial ds/dx(t) is equal to the gradient of the loss dl/dx(s(t))
         t: integer
-        a_dict: dictionary {index: sympy variable}
+        coefficient_dict: dictionary {index: sympy variable}
         dP: linear function
     
     This returns the following expression, which we interpret as being set equal to 0
         (dP(a0) + a1) + (dP(a1) + 2*a2)*t + (dP(a2) + 3*a3)*(t**2) + (dP(a3) + 4*a4)*(t**3) + dP(a4)*(t**4)
     """
     out = 0
-    for i in range(len(a_dict) - 1):
-        out += (dP(a_dict[i]) + (i+1)*a_dict[i+1])*(t**i)
-    out += dP(a_dict[len(a_dict) - 1]*(t**(len(a_dict) - 1)))
+    for i in range(len(coefficient_dict) - 1):
+        out += (dP(coefficient_dict[i]) + (i+1)*coefficient_dict[i+1])*(t**i)
+    out += dP(coefficient_dict[len(coefficient_dict) - 1]*(t**(len(coefficient_dict) - 1)))
     return out
 
 
-def evaluate_s(t_value, a_dict):
+def evaluate_s(t_value, coefficient_dict):
     """
-    Evaluate the polynomial s 
+    Evaluate the sympy polynomial s 
         t_value: a single number or sympy variable
-        a_dict: a dictionary {index: number or sympy variable}
+        coefficient_dict: a dictionary {index: number or sympy variable}
+    Return:
+        The output of evaluating the polynomial defined by coefficient_dict at t_value
     """
     out = 0
-    for i in range(len(a_dict)):
+    for i in range(len(coefficient_dict)):
         m = 1
         for j in range(i):
             m = m*t_value
-        out += a_dict[i]*m
+        out += coefficient_dict[i]*m
     return out
-
-
-def find_starting_value(equations, a_dict, raw_starting_value):
-    """
-    Given an underspecified set of equations with no starting value and a proposal starting value,
-      find a new starting value that the proposal value divides and that yields an integer solution
-    """
-    equations_with_raw_start = equations + [a_dict[0] - raw_starting_value]
-    raw_solution_values = list(linsolve(equations_with_raw_start, list(a_dict.values())))[0]
-    raw_rational_solution_dict = {a_var: value for a_var, value in zip(a_dict.values(), raw_solution_values)} 
-    raw_denominator_lcm = np.lcm.reduce([s.q for s in raw_rational_solution_dict.values()])
-    return raw_starting_value * raw_denominator_lcm
 
 
 def choose_solution(underspecified_solution_values):
     # Replace all of the variable terms in a list of sympy expressions with 1
     symbol_dict = {
-        x:sympy.Rational(np.random.randint(-10, 10))
+        x: sympy.Rational(np.random.randint(-10, 10))
         for x in underspecified_solution_values if isinstance(x, sympy.core.symbol.Symbol)}
-    print(symbol_dict)
     solution_values = []
     for x in list(underspecified_solution_values):
         if isinstance(x, sympy.core.numbers.Number):
@@ -87,20 +137,24 @@ def choose_solution(underspecified_solution_values):
     return solution_values
 
 
-def get_solution_dict(equations, a_dict):
+def get_solution_dict(equations, coefficient_dict):
     """
     Solve the system of diophantine equations by solving the linear system and upscaling the solution
         equations: A system of equations
-        a_dict: a dictionary {index: number or sympy variable}
+        coefficient_dict: a dictionary {index: number or sympy variable}
+    Returns:
+        A dictionary from sympy variable to value
     """
-    underspecified_solution_values = list(linsolve(equations, list(a_dict.values())))[0]
-    raw_rational_solution_values = choose_solution(underspecified_solution_values=underspecified_solution_values)
-    raw_rational_solution_dict = {a_var: value for a_var, value in zip(a_dict.values(), raw_rational_solution_values)} 
     
+    # Solve as a linear equation. Results may be rational
+    underspecified_solution_values = list(linsolve(equations, list(coefficient_dict.values())))[0]
+    raw_rational_solution_values = choose_solution(underspecified_solution_values=underspecified_solution_values)
+    raw_rational_solution_dict = {a_var: value for a_var, value in zip(coefficient_dict.values(), raw_rational_solution_values)} 
+    
+    # Convert rational solution to integer solution with least common multiple
     raw_denominator_lcm = np.lcm.reduce([s.q for s in raw_rational_solution_dict.values()])
-
     solution_values = [value*raw_denominator_lcm for value in raw_rational_solution_values]
-    solution_dict = {a_var: value for a_var, value in zip(a_dict.values(), solution_values)} 
+    solution_dict = {a_var: value for a_var, value in zip(coefficient_dict.values(), solution_values)} 
 
     # Verify the solution
     for equation in equations:
